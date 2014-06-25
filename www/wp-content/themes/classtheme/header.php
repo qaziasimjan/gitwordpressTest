23484c8e9b03c64324e913231e3a8ff2bd15b89e
rhildred <rhildred@gmail.com>
2014-06-25 08:19:51 -0400

    fiddling with design

--- www/wp-content/themes/classtheme/header.php
+++ www/wp-content/themes/classtheme/header.php
@@ -2,10 +2,11 @@
 <html>
 <head>
 <title>Tutorial theme</title>
-<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+<link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/css/style.css" />
+<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Tangerine">
 </head>
 <body>
 <div id="wrapper">
 <div id="header">
-<h1>HEADER</h1>
+<h1 class="mainHeading">HEADER</h1>
 </div>
