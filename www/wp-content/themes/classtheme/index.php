9bc5b5a0f601578ce647f262da489cdf0bdffa5f
rhildred <rhildred@gmail.com>
2014-06-25 09:12:11 -0400

    switched index and page, and gave them names so that they could be used as templates

--- www/wp-content/themes/classtheme/index.php
+++ www/wp-content/themes/classtheme/index.php
@@ -1,16 +1,16 @@
 <?php
+/*
+ Template Name: Page Template
+*/
 GitWordPressLayout::$Viewbag['sTitle'] = "Rich's blog";
 GitWordPressLayout::layout("_layout.php");
 ?>
 <div id="main">
 	<div id="content">
-		<h1>Main Area</h1>
+		<h1>Page Area</h1>
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 <h1><?php the_title(); ?></h1>
-		<h4>Posted on <?php the_time('F jS, Y') ?></h4>
 		<p><?php the_content(__('(more...)')); ?></p>
-		<hr> <?php endwhile; else: ?>
-<p><?php _e('Sorry, no posts matched your criteria.'); ?></p><?php endif; ?>
-</div>
-<?php get_sidebar(); ?>
+		<hr> <?php endwhile;endif?>
+		</div>
 </div>
